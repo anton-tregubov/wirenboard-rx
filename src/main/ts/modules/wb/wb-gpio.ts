661cb7b83d0936cda1fba10dedae9246b071db10
Anton Tregubov <tregubov@gmail.com>
2024-06-27 01:40:38 +0300

    add wb-gpio

--- src/main/ts/modules/wb/wb-gpio.ts
+++ src/main/ts/modules/wb/wb-gpio.ts
@@ -7,10 +7,11 @@ import {
   TopicSubscriptionConfig,
 } from '@main/modules/core/definitions'
 
-export const EXT_PREFIX = 'EXT'
+const EXT_PREFIX = 'EXT'
 const DI_WD_IN_SENSOR = 'IN'
 const DO_R10A_OUT_SENSOR = 'R3A'
 
+const PROPERTY_MODULE = 'module'
 const PROPERTY_DI_WD_IN = 'in'
 const PROPERTY_DO_R10A_OUT = 'out'
 
@@ -20,62 +21,146 @@ const DI_WD_14_FIRST_SENSOR_INCLUSIVE = 1
 const DO_R10A_8_LAST_SENSOR_EXCLUSIVE = 9
 const DO_R10A_8_FIRST_SENSOR_INCLUSIVE = 1
 
-type DiWdInTopicSubscriptionConfig<Index extends Integer> = TopicSubscriptionConfig<`${typeof PROPERTY_DI_WD_IN}${Index}`, typeof FIELD_DESTINY_READ, typeof TOPIC_VALUE_SWITCH>;
-type DoR10AOutTopicSubscriptionConfig<Index extends Integer> = TopicSubscriptionConfig<`${typeof PROPERTY_DO_R10A_OUT}${Index}`, typeof FIELD_DESTINY_READ_AND_WRITE, typeof TOPIC_VALUE_SWITCH>;
+type ModuleIndex = IntegerRange<1, 9>
 
-type DiWdInsTopicsSubscriptionConfig<Count extends Integer> = {
-  readonly [Index in Count as `${typeof DI_WD_IN_SENSOR}${Index}`]: DiWdInTopicSubscriptionConfig<Index>
+type ModuleDiWdInTopicSubscriptionConfig<Index extends ModuleIndex, Port extends Integer> = TopicSubscriptionConfig<`${typeof PROPERTY_MODULE}${Index}${typeof PROPERTY_DI_WD_IN}${Port}`, typeof FIELD_DESTINY_READ, typeof TOPIC_VALUE_SWITCH>;
+type ModuleDoR10AOutTopicSubscriptionConfig<Index extends ModuleIndex, Port extends Integer> = TopicSubscriptionConfig<`${typeof PROPERTY_MODULE}${Index}${typeof PROPERTY_DO_R10A_OUT}${Port}`, typeof FIELD_DESTINY_READ_AND_WRITE, typeof TOPIC_VALUE_SWITCH>;
+
+type ModuleDiWdInsTopicsSubscriptionConfig<Index extends ModuleIndex, PortCount extends Integer> = {
+  readonly [Port in PortCount as `${typeof EXT_PREFIX}${Index}_${typeof DI_WD_IN_SENSOR}${Port}`]: ModuleDiWdInTopicSubscriptionConfig<Index, Port>
 }
 
-type DoR10AOutsTopicsSubscriptionConfig<Count extends Integer> = {
-  readonly [Index in Count as `${typeof DO_R10A_OUT_SENSOR}${Index}`]: DoR10AOutTopicSubscriptionConfig<Index>
+type ModuleDoR10AOutsTopicsSubscriptionConfig<Index extends ModuleIndex, PortCount extends Integer> = {
+  readonly [Port in PortCount as `${typeof EXT_PREFIX}${Index}_${typeof DI_WD_IN_SENSOR}${Port}`]: ModuleDoR10AOutTopicSubscriptionConfig<Index, Port>
 }
 
-type DiWdPrototypeConfig<Count extends Integer> = DiWdInsTopicsSubscriptionConfig<Count>
-type DoR10APrototypeConfig<Count extends Integer> = DoR10AOutsTopicsSubscriptionConfig<Count>
+type ModuleDiWd14Config<Index extends ModuleIndex> = ModuleDiWdInsTopicsSubscriptionConfig<Index, IntegerRange<typeof DI_WD_14_FIRST_SENSOR_INCLUSIVE, typeof DI_WD_14_LAST_SENSOR_EXCLUSIVE>>
+type ModuleDoR10A8Config<Index extends ModuleIndex> = ModuleDoR10AOutsTopicsSubscriptionConfig<Index, IntegerRange<typeof DO_R10A_8_FIRST_SENSOR_INCLUSIVE, typeof DO_R10A_8_LAST_SENSOR_EXCLUSIVE>>
 
-type WbDiWd14Config = DiWdPrototypeConfig<IntegerRange<typeof DI_WD_14_FIRST_SENSOR_INCLUSIVE, typeof DI_WD_14_LAST_SENSOR_EXCLUSIVE>>
-type WbDoR10A8Config = DoR10APrototypeConfig<IntegerRange<typeof DO_R10A_8_FIRST_SENSOR_INCLUSIVE, typeof DO_R10A_8_LAST_SENSOR_EXCLUSIVE>>
+type WbGpioModuleConfigs<Index extends ModuleIndex> =
+  ModuleDiWd14Config<Index>
+  | ModuleDoR10A8Config<Index>
+  | EmptyModuleConfig
 
-export type WbDiWd14Device = PhysicalWbDevice<WbDiWd14Config>
-export type WbDoR10A8Device = PhysicalWbDevice<WbDoR10A8Config>
+type EmptyModuleConfig = NonNullable<unknown>
 
-function diWdInTopicSubscriptionConfig<Index extends Integer>(index: Index): DiWdInTopicSubscriptionConfig<Index> {
+type OrEmptyConfig<T> = T extends never ? EmptyModuleConfig : T
+
+export type WbGpioConfig<
+  Module1 extends WbGpioModuleConfigs<1>,
+  Module2 extends WbGpioModuleConfigs<2>,
+  Module3 extends WbGpioModuleConfigs<3>,
+  Module4 extends WbGpioModuleConfigs<4>,
+  Module5 extends WbGpioModuleConfigs<5>,
+  Module6 extends WbGpioModuleConfigs<6>,
+  Module7 extends WbGpioModuleConfigs<7>,
+  Module8 extends WbGpioModuleConfigs<8>,
+> = EmptyModuleConfig
+  & OrEmptyConfig<Module1>
+  & OrEmptyConfig<Module2>
+  & OrEmptyConfig<Module3>
+  & OrEmptyConfig<Module4>
+  & OrEmptyConfig<Module5>
+  & OrEmptyConfig<Module6>
+  & OrEmptyConfig<Module7>
+  & OrEmptyConfig<Module8>
+
+function moduleDiWdInTopicSubscriptionConfig<Index extends ModuleIndex, Port extends Integer>(index: Index, port: Port): ModuleDiWdInTopicSubscriptionConfig<Index, Port> {
   return {
-    fieldBaseName: `${PROPERTY_DI_WD_IN}${index}`,
+    fieldBaseName: `${PROPERTY_MODULE}${index}${PROPERTY_DI_WD_IN}${port}`,
     fieldDestiny: FIELD_DESTINY_READ,
     fieldValueType: TOPIC_VALUE_SWITCH,
   }
 }
 
-function doR10AOutTopicSubscriptionConfig<Index extends Integer>(index: Index): DoR10AOutTopicSubscriptionConfig<Index> {
+function moduleDoR10AOutTopicSubscriptionConfig<Index extends ModuleIndex, Port extends Integer>(index: Index, port: Port): ModuleDoR10AOutTopicSubscriptionConfig<Index, Port> {
   return {
-    fieldBaseName: `${PROPERTY_DO_R10A_OUT}${index}`,
+    fieldBaseName: `${PROPERTY_MODULE}${index}${PROPERTY_DO_R10A_OUT}${port}`,
     fieldDestiny: FIELD_DESTINY_READ_AND_WRITE,
     fieldValueType: TOPIC_VALUE_SWITCH,
   }
 }
 
-function diWdInsTopicSubscriptionConfig<F extends Integer, T extends Integer, Index extends IntegerRange<F, T>>(from: F, to: T): DiWdInsTopicsSubscriptionConfig<Index> {
+function moduleDiWdInsTopicSubscriptionConfig<Index extends ModuleIndex, F extends Integer, T extends Integer, Port extends IntegerRange<F, T>>(moduleIndex: Index, from: F, to: T): ModuleDiWdInsTopicsSubscriptionConfig<Index, Port> {
   return [...range(from, to)]
     .reduce(
-      (collector, index) => Object.assign(collector, { [`${DI_WD_IN_SENSOR}${index}`]: diWdInTopicSubscriptionConfig(+index) }),
-      {} as DiWdInsTopicsSubscriptionConfig<Index>)
+      (collector, port) => Object.assign(collector, { [`${EXT_PREFIX}${moduleIndex}_${DI_WD_IN_SENSOR}${port}`]: moduleDiWdInTopicSubscriptionConfig(moduleIndex, +port) }),
+      {} as ModuleDiWdInsTopicsSubscriptionConfig<Index, Port>)
 }
 
-function doR10AOutsTopicSubscriptionConfig<F extends Integer, T extends Integer, Index extends IntegerRange<F, T>>(from: F, to: T): DoR10AOutsTopicsSubscriptionConfig<Index> {
+function moduleDoR10AOutsTopicSubscriptionConfig<Index extends ModuleIndex, F extends Integer, T extends Integer, Port extends IntegerRange<F, T>>(moduleIndex: Index, from: F, to: T): ModuleDoR10AOutsTopicsSubscriptionConfig<Index, Port> {
   return [...range(from, to)]
     .reduce(
-      (collector, index) => Object.assign(collector, { [`${DO_R10A_OUT_SENSOR}${index}`]: doR10AOutTopicSubscriptionConfig(+index) }),
-      {} as DoR10AOutsTopicsSubscriptionConfig<Index>)
+      (collector, port) => Object.assign(collector, { [`${EXT_PREFIX}${moduleIndex}_${DO_R10A_OUT_SENSOR}${port}`]: moduleDoR10AOutTopicSubscriptionConfig(moduleIndex, +port) }),
+      {} as ModuleDoR10AOutsTopicsSubscriptionConfig<Index, Port>)
 }
 
-export const WB_DI_WD_14_CONFIG: WbDiWd14Config = {
-  ...diWdInsTopicSubscriptionConfig(DI_WD_14_FIRST_SENSOR_INCLUSIVE, DI_WD_14_LAST_SENSOR_EXCLUSIVE),
+export const GPIO_MODULE_DI_WD_14 = 'di-wd-14'
+export const GPIO_MODULE_DO_R10_8 = 'di-wd-8'
+export const GPIO_MODULE_NOT_EXISTS = ''
+
+export type WbGpioModule = typeof GPIO_MODULE_DI_WD_14 | typeof GPIO_MODULE_DO_R10_8 | typeof GPIO_MODULE_NOT_EXISTS
+
+type ModuleToConfigMapper<Index extends ModuleIndex> = {
+  [GPIO_MODULE_DI_WD_14]: ModuleDiWd14Config<Index>
+  [GPIO_MODULE_DO_R10_8]: ModuleDoR10A8Config<Index>
+  [GPIO_MODULE_NOT_EXISTS]: EmptyModuleConfig
 }
 
-export const WB_DO_R10A_8_CONFIG: WbDoR10A8Config = {
-  ...doR10AOutsTopicSubscriptionConfig(DO_R10A_8_FIRST_SENSOR_INCLUSIVE, DO_R10A_8_LAST_SENSOR_EXCLUSIVE),
+export type WbGpioDevice<
+  Module1 extends WbGpioModule,
+  Module2 extends WbGpioModule,
+  Module3 extends WbGpioModule,
+  Module4 extends WbGpioModule,
+  Module5 extends WbGpioModule,
+  Module6 extends WbGpioModule,
+  Module7 extends WbGpioModule,
+  Module8 extends WbGpioModule,
+> = PhysicalWbDevice<
+  WbGpioConfig<
+    ModuleToConfigMapper<1>[Module1],
+    ModuleToConfigMapper<2>[Module2],
+    ModuleToConfigMapper<3>[Module3],
+    ModuleToConfigMapper<4>[Module4],
+    ModuleToConfigMapper<5>[Module5],
+    ModuleToConfigMapper<6>[Module6],
+    ModuleToConfigMapper<7>[Module7],
+    ModuleToConfigMapper<8>[Module8]
+  >
+>
+
+export function wbGpioConfig<
+  Modules extends [
+    WbGpioModule,
+    WbGpioModule,
+    WbGpioModule,
+    WbGpioModule,
+    WbGpioModule,
+    WbGpioModule,
+    WbGpioModule,
+    WbGpioModule
+  ]
+>(modules: Modules): WbGpioConfig<
+  ModuleToConfigMapper<1>[typeof modules[0]],
+  ModuleToConfigMapper<2>[typeof modules[1]],
+  ModuleToConfigMapper<3>[typeof modules[2]],
+  ModuleToConfigMapper<4>[typeof modules[3]],
+  ModuleToConfigMapper<5>[typeof modules[4]],
+  ModuleToConfigMapper<6>[typeof modules[5]],
+  ModuleToConfigMapper<7>[typeof modules[6]],
+  ModuleToConfigMapper<8>[typeof modules[7]]
+> {
+  return modules.reduce((collector, module, index) => {
+    const moduleIndex = (index + 1) as ModuleIndex
+    switch (module) {
+      case GPIO_MODULE_DI_WD_14:
+        return Object.assign(collector, moduleDiWdInsTopicSubscriptionConfig(moduleIndex, DI_WD_14_FIRST_SENSOR_INCLUSIVE, DI_WD_14_LAST_SENSOR_EXCLUSIVE))
+      case GPIO_MODULE_DO_R10_8:
+        return Object.assign(collector, moduleDoR10AOutsTopicSubscriptionConfig(moduleIndex, DO_R10A_8_FIRST_SENSOR_INCLUSIVE, DO_R10A_8_LAST_SENSOR_EXCLUSIVE))
+    }
+    return collector
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  }, {} as WbGpioConfig<any, any, any, any, any, any, any, any>)
 }
 
 export const WB_GPIO_TOPIC_IDENTIFIER = 'wb-gpio'
--- src/main/ts/modules/wb/wirenboard-device-factory.ts
+++ src/main/ts/modules/wb/wirenboard-device-factory.ts
@@ -1,11 +1,6 @@
 import { WB_MR6C_CONFIG, WB_MR6C_TOPIC_IDENTIFIER, WbMr6cDevice } from '@main/modules/wb/wb-mrc'
 import { ModbusDeviceFactory } from '@main/modules/core/modbus-device-factory'
-import {
-  CONTROLS_TOPIC_IDENTIFIER,
-  DEVICES_TOPIC_IDENTIFIER,
-  GpioDeviceIndex,
-  ModbusSlave,
-} from '@main/modules/core/definitions'
+import { CONTROLS_TOPIC_IDENTIFIER, DEVICES_TOPIC_IDENTIFIER, ModbusSlave } from '@main/modules/core/definitions'
 import { WB_MIR_CONFIG, WB_MIR_TOPIC_IDENTIFIER, WbMirDevice } from '@main/modules/wb/wb-mir'
 import { WB_MSW_CONFIG, WB_MSW_TOPIC_IDENTIFIER, WbMswDevice } from '@main/modules/wb/wb-msw'
 import { WB_MWAC_CONFIG, WB_MWAC_TOPIC_IDENTIFIER, WbMWacDevice } from '@main/modules/wb/wb-mwac'
@@ -13,12 +8,11 @@ import { WB_M1W2_CONFIG, WB_M1W2_TOPIC_IDENTIFIER, WbM1W2Device } from '@main/mo
 import { TopicName } from '@main/core/topic-events-stream-reactive-switch'
 import { Optional } from '@main/core/utils'
 import {
-  EXT_PREFIX,
-  WB_DI_WD_14_CONFIG,
-  WB_DO_R10A_8_CONFIG,
+  GPIO_MODULE_NOT_EXISTS,
   WB_GPIO_TOPIC_IDENTIFIER,
-  WbDiWd14Device,
-  WbDoR10A8Device,
+  wbGpioConfig,
+  WbGpioDevice,
+  WbGpioModule,
 } from '@main/modules/wb/wb-gpio'
 
 export interface WirenboardDeviceFactory {
@@ -32,9 +26,137 @@ export interface WirenboardDeviceFactory {
 
   createWbM1W2(slave: ModbusSlave): WbM1W2Device
 
-  createWbDiWd14(index: GpioDeviceIndex): WbDiWd14Device
+  createWbGpio<
+    M1 extends WbGpioModule
+  >(m1: M1): WbGpioDevice<
+    M1,
+    typeof GPIO_MODULE_NOT_EXISTS,
+    typeof GPIO_MODULE_NOT_EXISTS,
+    typeof GPIO_MODULE_NOT_EXISTS,
+    typeof GPIO_MODULE_NOT_EXISTS,
+    typeof GPIO_MODULE_NOT_EXISTS,
+    typeof GPIO_MODULE_NOT_EXISTS,
+    typeof GPIO_MODULE_NOT_EXISTS
+  >
 
-  createWbDoR10A8(index: GpioDeviceIndex): WbDoR10A8Device
+  createWbGpio<
+    M1 extends WbGpioModule,
+    M2 extends WbGpioModule
+  >(m1: M1, m2: M2): WbGpioDevice<
+    M1,
+    M2,
+    typeof GPIO_MODULE_NOT_EXISTS,
+    typeof GPIO_MODULE_NOT_EXISTS,
+    typeof GPIO_MODULE_NOT_EXISTS,
+    typeof GPIO_MODULE_NOT_EXISTS,
+    typeof GPIO_MODULE_NOT_EXISTS,
+    typeof GPIO_MODULE_NOT_EXISTS
+  >
+
+  createWbGpio<
+    M1 extends WbGpioModule,
+    M2 extends WbGpioModule,
+    M3 extends WbGpioModule
+  >(m1: M1, m2: M2, m3: M3): WbGpioDevice<
+    M1,
+    M2,
+    M3,
+    typeof GPIO_MODULE_NOT_EXISTS,
+    typeof GPIO_MODULE_NOT_EXISTS,
+    typeof GPIO_MODULE_NOT_EXISTS,
+    typeof GPIO_MODULE_NOT_EXISTS,
+    typeof GPIO_MODULE_NOT_EXISTS
+  >
+
+  createWbGpio<
+    M1 extends WbGpioModule,
+    M2 extends WbGpioModule,
+    M3 extends WbGpioModule,
+    M4 extends WbGpioModule
+  >(m1: M1, m2: M2, m3: M3, m4: M4): WbGpioDevice<
+    M1,
+    M2,
+    M3,
+    M4,
+    typeof GPIO_MODULE_NOT_EXISTS,
+    typeof GPIO_MODULE_NOT_EXISTS,
+    typeof GPIO_MODULE_NOT_EXISTS,
+    typeof GPIO_MODULE_NOT_EXISTS
+  >
+
+  createWbGpio<
+    M1 extends WbGpioModule,
+    M2 extends WbGpioModule,
+    M3 extends WbGpioModule,
+    M4 extends WbGpioModule,
+    M5 extends WbGpioModule
+  >(m1: M1, m2: M2, m3: M3, m4: M4, m5: M5): WbGpioDevice<
+    M1,
+    M2,
+    M3,
+    M4,
+    M5,
+    typeof GPIO_MODULE_NOT_EXISTS,
+    typeof GPIO_MODULE_NOT_EXISTS,
+    typeof GPIO_MODULE_NOT_EXISTS
+  >
+
+  createWbGpio<
+    M1 extends WbGpioModule,
+    M2 extends WbGpioModule,
+    M3 extends WbGpioModule,
+    M4 extends WbGpioModule,
+    M5 extends WbGpioModule,
+    M6 extends WbGpioModule
+  >(m1: M1, m2: M2, m3: M3, m4: M4, m5: M5, m6: M6): WbGpioDevice<
+    M1,
+    M2,
+    M3,
+    M4,
+    M5,
+    M6,
+    typeof GPIO_MODULE_NOT_EXISTS,
+    typeof GPIO_MODULE_NOT_EXISTS
+  >
+
+  createWbGpio<
+    M1 extends WbGpioModule,
+    M2 extends WbGpioModule,
+    M3 extends WbGpioModule,
+    M4 extends WbGpioModule,
+    M5 extends WbGpioModule,
+    M6 extends WbGpioModule,
+    M7 extends WbGpioModule
+  >(m1: M1, m2: M2, m3: M3, m4: M4, m5: M5, m6: M6, m7: M7): WbGpioDevice<
+    M1,
+    M2,
+    M3,
+    M4,
+    M5,
+    M6,
+    M7,
+    typeof GPIO_MODULE_NOT_EXISTS
+  >
+
+  // createWbGpio<
+  //   M1 extends WbGpioModule,
+  //   M2 extends WbGpioModule,
+  //   M3 extends WbGpioModule,
+  //   M4 extends WbGpioModule,
+  //   M5 extends WbGpioModule,
+  //   M6 extends WbGpioModule,
+  //   M7 extends WbGpioModule,
+  //   M8 extends WbGpioModule
+  // >(m1: M1, m2: M2, m3: M3, m4: M4, m5: M5, m6: M6, m7: M7, m8: M8): WbGpioDevice<
+  //   M1,
+  //   M2,
+  //   M3,
+  //   M4,
+  //   M5,
+  //   M6,
+  //   M7,
+  //   M8
+  // >
 }
 
 export class WirenboardDeviceFactoryImpl implements WirenboardDeviceFactory {
@@ -48,9 +170,9 @@ export class WirenboardDeviceFactoryImpl implements WirenboardDeviceFactory {
     return suffix => `/${DEVICES_TOPIC_IDENTIFIER}/${identifier}_${slave}` + (suffix ? `/${CONTROLS_TOPIC_IDENTIFIER}/${suffix}` : '')
   }
 
-  private creteGpioTopicSuffixResolver(index: GpioDeviceIndex): (suffix: Optional<string>) => TopicName {
-    return suffix => `/${DEVICES_TOPIC_IDENTIFIER}/${WB_GPIO_TOPIC_IDENTIFIER}` + (suffix ? `/${CONTROLS_TOPIC_IDENTIFIER}/${EXT_PREFIX}${index}_${suffix}` : '')
-  }
+  // private creteGpioTopicSuffixResolver(index: GpioDeviceIndex): (suffix: Optional<string>) => TopicName {
+  //   return suffix => `/${DEVICES_TOPIC_IDENTIFIER}/${WB_GPIO_TOPIC_IDENTIFIER}` + (suffix ? `/${CONTROLS_TOPIC_IDENTIFIER}/${EXT_PREFIX}${index}_${suffix}` : '')
+  // }
 
   createWbMr6c(slave: ModbusSlave): WbMr6cDevice {
     return this._modbusDeviceFactory.createPhysicalWbDevice(WB_MR6C_CONFIG, this.creteModbusTopicSuffixResolver(WB_MR6C_TOPIC_IDENTIFIER, slave))
@@ -72,11 +194,28 @@ export class WirenboardDeviceFactoryImpl implements WirenboardDeviceFactory {
     return this._modbusDeviceFactory.createPhysicalWbDevice(WB_M1W2_CONFIG, this.creteModbusTopicSuffixResolver(WB_M1W2_TOPIC_IDENTIFIER, slave))
   }
 
-  createWbDiWd14(index: GpioDeviceIndex): WbDiWd14Device {
-    return this._modbusDeviceFactory.createPhysicalWbDevice(WB_DI_WD_14_CONFIG, this.creteGpioTopicSuffixResolver(index))
-  }
-
-  createWbDoR10A8(index: GpioDeviceIndex): WbDoR10A8Device {
-    return this._modbusDeviceFactory.createPhysicalWbDevice(WB_DO_R10A_8_CONFIG, this.creteGpioTopicSuffixResolver(index))
+  createWbGpio<M1 extends WbGpioModule>(m1: M1): WbGpioDevice<M1, typeof GPIO_MODULE_NOT_EXISTS, typeof GPIO_MODULE_NOT_EXISTS, typeof GPIO_MODULE_NOT_EXISTS, typeof GPIO_MODULE_NOT_EXISTS, typeof GPIO_MODULE_NOT_EXISTS, typeof GPIO_MODULE_NOT_EXISTS, typeof GPIO_MODULE_NOT_EXISTS>
+  createWbGpio<M1 extends WbGpioModule, M2 extends WbGpioModule>(m1: M1, m2: M2): WbGpioDevice<M1, M2, typeof GPIO_MODULE_NOT_EXISTS, typeof GPIO_MODULE_NOT_EXISTS, typeof GPIO_MODULE_NOT_EXISTS, typeof GPIO_MODULE_NOT_EXISTS, typeof GPIO_MODULE_NOT_EXISTS, typeof GPIO_MODULE_NOT_EXISTS>
+  createWbGpio<M1 extends WbGpioModule, M2 extends WbGpioModule, M3 extends WbGpioModule>(m1: M1, m2: M2, m3: M3): WbGpioDevice<M1, M2, M3, typeof GPIO_MODULE_NOT_EXISTS, typeof GPIO_MODULE_NOT_EXISTS, typeof GPIO_MODULE_NOT_EXISTS, typeof GPIO_MODULE_NOT_EXISTS, typeof GPIO_MODULE_NOT_EXISTS>
+  createWbGpio<M1 extends WbGpioModule, M2 extends WbGpioModule, M3 extends WbGpioModule, M4 extends WbGpioModule>(m1: M1, m2: M2, m3: M3, m4: M4): WbGpioDevice<M1, M2, M3, M4, typeof GPIO_MODULE_NOT_EXISTS, typeof GPIO_MODULE_NOT_EXISTS, typeof GPIO_MODULE_NOT_EXISTS, typeof GPIO_MODULE_NOT_EXISTS>
+  createWbGpio<M1 extends WbGpioModule, M2 extends WbGpioModule, M3 extends WbGpioModule, M4 extends WbGpioModule, M5 extends WbGpioModule>(m1: M1, m2: M2, m3: M3, m4: M4, m5: M5): WbGpioDevice<M1, M2, M3, M4, M5, typeof GPIO_MODULE_NOT_EXISTS, typeof GPIO_MODULE_NOT_EXISTS, typeof GPIO_MODULE_NOT_EXISTS>
+  createWbGpio<M1 extends WbGpioModule, M2 extends WbGpioModule, M3 extends WbGpioModule, M4 extends WbGpioModule, M5 extends WbGpioModule, M6 extends WbGpioModule>(m1: M1, m2: M2, m3: M3, m4: M4, m5: M5, m6: M6): WbGpioDevice<M1, M2, M3, M4, M5, M6, typeof GPIO_MODULE_NOT_EXISTS, typeof GPIO_MODULE_NOT_EXISTS>
+  createWbGpio<M1 extends WbGpioModule, M2 extends WbGpioModule, M3 extends WbGpioModule, M4 extends WbGpioModule, M5 extends WbGpioModule, M6 extends WbGpioModule, M7 extends WbGpioModule>(m1: M1, m2: M2, m3: M3, m4: M4, m5: M5, m6: M6, m7: M7): WbGpioDevice<M1, M2, M3, M4, M5, M6, M7, typeof GPIO_MODULE_NOT_EXISTS>
+  // createWbGpio<M1 extends WbGpioModule, M2 extends WbGpioModule, M3 extends WbGpioModule, M4 extends WbGpioModule, M5 extends WbGpioModule, M6 extends WbGpioModule, M7 extends WbGpioModule, M8 extends WbGpioModule>(m1: M1, m2: M2, m3: M3, m4: M4, m5: M5, m6: M6, m7: M7, m8: M8): WbGpioDevice<M1, M2, M3, M4, M5, M6, M7, M8>
+  // createWbGpio(m1: WbGpioModule, m2?: WbGpioModule, m3?: WbGpioModule, m4?: WbGpioModule, m5?: WbGpioModule, m6?: WbGpioModule, m7?: WbGpioModule, m8?: WbGpioModule): WbGpioDevice<WbGpioModule, WbGpioModule, WbGpioModule, WbGpioModule, WbGpioModule, WbGpioModule, WbGpioModule, WbGpioModule> {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  createWbGpio(m1: any, m2?: any, m3?: any, m4?: any, m5?: any, m6?: any, m7?: any, m8?: any): any {
+    const modules: WbGpioModule[] = [m1]
+    if (m2) modules.push(m2)
+    if (m3) modules.push(m3)
+    if (m4) modules.push(m4)
+    if (m5) modules.push(m5)
+    if (m6) modules.push(m6)
+    if (m7) modules.push(m7)
+    if (m8) modules.push(m8)
+    const config = wbGpioConfig(modules as [WbGpioModule, WbGpioModule, WbGpioModule, WbGpioModule, WbGpioModule, WbGpioModule, WbGpioModule, WbGpioModule])
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    return this._modbusDeviceFactory.createPhysicalWbDevice(config, suffix => `/${DEVICES_TOPIC_IDENTIFIER}/${WB_GPIO_TOPIC_IDENTIFIER}` + (suffix ? `/${CONTROLS_TOPIC_IDENTIFIER}/${suffix}` : ''))
   }
 }
